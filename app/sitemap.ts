93c4688c60ef32aab259e91726e34197d32ddeb0
morotamorotamorota <e1q23082@oit.ac.jp>
2026-02-23 02:55:54 +0900

    名前検索の候補追加

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next';
 import { supabase } from '../lib/supabase'; // パスが違う場合は微調整してください
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseUrl = 'https://SEARCRATE-smoky.vercel.app';
+  const baseUrl = 'https://searcrate.com';
 
   // ① Supabaseから登録されているすべてのツールを取得
   const { data: tools } = await supabase
